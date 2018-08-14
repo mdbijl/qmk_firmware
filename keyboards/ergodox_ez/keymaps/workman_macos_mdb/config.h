737ff644383bb5720e06be93882d15c0243b97e5
Maurice @ Edison <maurice@conveniencefactory.com>
2018-08-14 19:52:00 +0200

    Custom config

---
+++ keyboards/ergodox_ez/keymaps/workman_macos_mdb/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_WORKMANMACOSMDB_H
+#define CONFIG_WORKMANMACOSMDB_H
+
+#include "../../config.h"
+
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2 // Nr of taps before toggling to layer -- TT()
+
+#endif

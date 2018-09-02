80b98e3fbe02e2ddd28f8044fd63090df60d548d
Maurice @ Ampere <maurice@conveniencefactory.com>
2018-09-02 13:42:43 +0200

    Other startup animation, added mouse settings

--- keyboards/kinesis/keymaps/workman_macos_mdb/config.h
+++ keyboards/kinesis/keymaps/workman_macos_mdb/config.h
@@ -3,6 +3,17 @@
 
 #include "../../config.h"
 
-// place overrides here
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2 // Nr of taps before toggling to layer -- TT()
+
+#define MOUSEKEY_INTERVAL       20
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    15
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 
 #endif
